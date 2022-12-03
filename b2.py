53a0c94370c2b4caf665d00de1fed4035ed4d211
Phạm Phước Bảo Tín <115201730+BAOTIN2004@users.noreply.github.com>
2022-12-04 00:01:27 +0700

    Add files via upload
    Signed-off-by: Phạm Phước Bảo Tín <115201730+BAOTIN2004@users.noreply.github.com>

---
+++ b2.py
@@ -0,0 +1,10 @@
+a=open('test.txt',mode="r+")
+a=open('test.txt')
+a=open('test.txt',mode="r+b")
+a=open('test.txt',mode='r',encoding='utf8')
+print(a.read())
+a.close()
+
+
+b=open('BL.jpg',mode="r+b")
+print(b.read())
